f00b4865d4842d310fc27d40e8d2912259fb2559
benjamin <benji4genius@gmail.com>
2024-12-28 01:00:00 -0500

    Pushing comments

--- src/main/java/com/ecommerce/store/service/CategoryService.java
+++ src/main/java/com/ecommerce/store/service/CategoryService.java
@@ -21,7 +21,11 @@ public class CategoryService {
       return categoryRepo.findAll();
 
     }
-
+    /* This method is called the categoryController class, and it uses to
+    execute the category and if you look closely this is a direct replica of
+    the category entity in the model, and it is being called by calling the getters,
+     and the setter methods in the category in the model folder*/
+    
     public void editCategory(int categoryId, Category updateCategory) {
         Category category = categoryRepo.getById(categoryId);
         category.setCategoryName(updateCategory.getCategoryName());
